1bb99a1f4ae349464c0a952281a43023577e7159
Zadock Maloba <malobazadock@gmail.com>
2024-06-02 17:01:50 +0300

    Link tests to libc and libav

--- build.zig
+++ build.zig
@@ -15,20 +15,6 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "Zig-FFMPEG",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
-
     const exe = b.addExecutable(.{
         .name = "Zig-FFMPEG",
         .root_source_file = b.path("src/main.zig"),
@@ -74,19 +60,20 @@ pub fn build(b: *std.Build) void {
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+    exe_unit_tests.linkSystemLibrary("c");
+    exe_unit_tests.linkSystemLibrary("zlib");
+    exe_unit_tests.linkSystemLibrary("avcodec");
+    exe_unit_tests.linkSystemLibrary("avformat");
+    exe_unit_tests.linkSystemLibrary("avutil");
+    exe_unit_tests.linkSystemLibrary("avdevice");
+    exe_unit_tests.linkSystemLibrary("avfilter");
+    exe_unit_tests.linkLibC();
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
@@ -94,6 +81,5 @@ pub fn build(b: *std.Build) void {
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
