9da67bcf7bc6a943e22f5ff16ad082188fc670ef
Zadock Maloba <malobazadock@gmail.com>
2024-06-02 17:02:15 +0300

    Create first test

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,13 @@ const std = @import("std");
 const avio_list_dir = @import("example/avio_list_dir.zig");
 
 pub fn main() !u8 {
+
+    //AVIO list directory contents2
     _ = avio_list_dir.exec();
 
     return 0;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+test "AVIO List Directories" {
+    _ = avio_list_dir.exec();
 }
